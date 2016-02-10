b50bee218697aade1983ceb82eaf6b40c5a3324c
Marty-Scott Sainty (NWIS - Software Development) <Ma166588@wales.nhs.uk>
2016-02-10 12:02:22 +0000

    updates

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# nutchmetarobots
-A plugin developed for Apache Nutch 2.3 to honour the robots meta tag at index time. 
-#
+# Apache Nutch 2.3 - Meta Robots - Plugin
+A plugin developed for Apache Nutch 2.3 to honour the robots meta tag at index time. Apache Nutch 2.3 currenlty ignores the ``noindex`` rule and proceeds to index the content to Solr. This plugin has been developed to discard the document from index if it contains the robots metatag with the associated ``noindex`` rule.
+
+##Usage
+
+```
+<property>
+    <name>plugin.includes</name>
+    <value>metarobots|indexer-solr|scoring-opic|urlnormalizer-(pass|regex|basic)|protocol-(http|https)|urlfilter-regex|parse-(html|metatags|tika)|index-(basic|metadata|more)</value>
+</property>
+```
+
+##How To Build
+
+Before proceeding make sure you have ``apache ant`` installed. 
+
+In order to use this plugin you'll need to build it into Apache Nutch 2.3.
+
+Follow these steps to ensure your plugin gets built into the runtime folder:
+
+1. Copy the plugin files to the ``NUTCH_ROOT/src/plugin`` folder. 
+
+2. Open ``NUTCH_ROOT/src/plugin/build.xml`` and add the following children to the associated target elements:  
+    ```
+        <target name="deploy">
+            <ant dir="metarobots" target="deploy"/>
+        </target>
+        
+        <target name="test">
+            <ant dir="metarobots" target="test"/>
+        </target>
+        
+        <target name="clean">
+            <ant dir="metarobots" target="clean"/>
+        </target>
+    ```
+
+    **Do NOT overwrite any existing ```<ant>``` elements**
+    
+3. Open a terminal and navigate to the ``NUTCH_ROOT`` directory.
+
+   Enter the following command:
+   
+   ```ant runtime```
+   
+
---
+++ apache-nutch-2.3/src/plugin/metarobots/src/java/saintybalboa/nutch/metarobots/MetaRobotsIndexer.java
@@ -0,0 +1,70 @@
+package saintybalboa.nutch.metarobots;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Arrays;
+import java.util.List;
+
+import org.mortbay.log.Log;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.oro.text.regex.*;
+import org.apache.nutch.util.NutchConfiguration;
+import org.apache.nutch.util.URLUtil;
+import org.apache.nutch.util.domain.DomainSuffix;
+
+import org.apache.avro.util.Utf8;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.indexer.IndexingException;
+import org.apache.nutch.indexer.IndexingFilter;
+import org.apache.nutch.indexer.NutchDocument;
+import org.apache.nutch.storage.WebPage;
+
+//Commons Logging imports
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MetaRobotsIndexer implements IndexingFilter {
+	
+	public static final Logger LOG = LoggerFactory.getLogger(MetaRobotsIndexer.class);
+	
+    private Configuration conf;
+    
+    private Collection<WebPage.Field> fields = new HashSet<WebPage.Field>();
+
+    
+    
+    @Override
+    public NutchDocument filter(NutchDocument document, String url, WebPage webPage) throws IndexingException {
+    	
+		 String meta_robots = document.getFieldValue("meta_robots");
+		 
+         return (meta_robots != null && meta_robots.toString().toLowerCase().contains("noindex")) ? null : document;
+    }
+
+ 
+
+    @Override
+    public void setConf(Configuration conf) {
+        this.conf = conf;
+    }
+
+    @Override
+    public Configuration getConf() {
+        return this.conf;
+    }
+
+    @Override
+    public Collection<WebPage.Field> getFields() {
+        return new HashSet<WebPage.Field>();
+    }
+    
+    
+    
+    public static void main(String[] args) throws Exception {
+
+    }
+}
